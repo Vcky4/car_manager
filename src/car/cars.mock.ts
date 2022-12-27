da20af020dfd10ba72deff445191f02f16725084
Vcky4 <okon.victor.u@gmail.com>
2022-12-27 12:39:12 +0100

    cars mock data

---
+++ src/car/cars.mock.ts
@@ -0,0 +1,37 @@
+export const CARS = [
+    {
+        id: 1,
+        name: 'BMW',
+        price: 10000,
+        description: 'BMW is a German automobile, motorcycle and engine manufacturing company founded in 1916. It is one of the best-selling luxury vehicle brands in the world, and is among the most valuable automotive brands. BMW is a member of the "German Big 3" luxury automakers, along with Audi and Mercedes-Benz, which are the three best-selling luxury automakers in the world.',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/0/0e/BMW_Logo_%282017%29.svg/1200px-BMW_Logo_%282017%29.svg.png',
+    },
+    {
+        id: 2,
+        name: 'Mercedes',
+        price: 8000,
+        description: 'Mercedes-Benz is a German global automobile marque and a division of Daimler AG. The brand is known for luxury vehicles, buses, coaches, and lorries. The headquarters is in Stuttgart, Baden-Württemberg. The name first appeared in 1926 under Daimler-Benz. In 2018, Mercedes-Benz was the biggest selling premium vehicle brand in the world, having sold 2.31 million passenger cars.',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Mercedes-Benz_logo.svg/1200px-Mercedes-Benz_logo.svg.png',
+    },
+    {
+        id: 3,
+        name: 'Audi',
+        price: 12000,
+        description: 'Audi AG is a German automobile manufacturer that designs, engineers, produces, markets and distributes luxury vehicles. Audi oversees worldwide operations from its headquarters in Ingolstadt, Bavaria, Germany. Audi-branded vehicles are produced in nine production facilities worldwide. The origins of the company are complex, going back to the early 20th century and the initial enterprises (Horch and the Audiwerke) founded by engineer August Horch; and two other manufacturers (DKW and Wanderer), leading to the foundation of Auto Union in 1932. The modern era of Audi essentially began in the 1960s when Auto Union was acquired by Volkswagen from Daimler-Benz. After relaunching the Audi brand with the 1965 introduction of the Audi F103 series, Volkswagen merged Auto Union with NSU Motorenwerke in 1969, thus creating the present day form of the company.',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Audi_Logo.svg/1200px-Audi_Logo.svg.png',
+    },
+    {
+        id: 4,
+        name: 'Porsche',
+        price: 20000,
+        description: 'Dr. Ing. h.c. F. Porsche AG, usually shortened to Porsche AG, is a German automobile manufacturer specializing in high-performance sports cars, SUVs and sedans. Porsche AG is headquartered in Stuttgart, and is owned by Volkswagen AG, a controlling stake of which is owned by Porsche Automobil Holding SE. Porsche AG is the largest and most traditional Sports Car manufacturer and the most profitable automotive manufacturer in the world. Porsche AG is the largest automaker and most profitable manufacturer of SUVs and the most profitable manufacturer of luxury vehicles in the world. Porsche AG is the largest automaker in the world by volume.',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Porsche_Logo.svg/1200px-Porsche_Logo.svg.png',
+    },
+    {
+        id: 5,
+        name: 'Lamborghini',
+        price: 30000,
+        description: 'Automobili Lamborghini S.p.A. is an Italian brand and manufacturer of luxury sports cars and SUVs based in SantAgata Bolognese. The company is owned by the Volkswagen Group through its subsidiary Audi. Lamborghini\'s production facility and headquarters are located in the Italian town of SantAgata Bolognese, where it was founded in 1963. In 2011, Lamborghini\'s 831 employees produced 1,711 vehicles. Lamborghini produces sports cars and V12 engines for offshore powerboat racing. Lamborghini currently produces the V12-powered Aventador and the V10-powered Huracán. In 2015, Lamborghini\'s sales reached 3,457 cars, which was a 43% increase from the previous year. Lamborghini sold 2,530 cars in 2016, including the Urus, which was unveiled at the 2012 Beijing Auto Show. Lamborghini has also produced a limited number of racing cars, such as the LM002 and the Sesto Elemento.',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Lamborghini_Logo.svg/1200px-Lamborghini_Logo.svg.png',
+    },
+];
