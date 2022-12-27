7b0bd17c98ddb449f3c46e80061f0e0e567b03e4
Vcky4 <okon.victor.u@gmail.com>
2022-12-27 12:30:24 +0100

    get cars

--- src/car/car.controller.ts
+++ src/car/car.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { CarService } from './car.service';
 
 @Controller('car')
 export class CarController {
     constructor(private readonly carService: CarService) {}
+
+    @Get()
+    getCars() {
+        return this.carService.getCars();
+    }
 }
