1d003ac5e694c64069d7ea64336a0ada91acf223
Vcky4 <okon.victor.u@gmail.com>
2022-12-27 18:22:14 +0100

    get car by id

--- src/car/car.service.ts
+++ src/car/car.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpException } from '@nestjs/common';
 import { CARS } from './cars.mock';
 @Injectable()
 export class CarService {
@@ -9,13 +9,18 @@ export class CarService {
     public postCar(car) {
         this.cars.push(car);
     }
-    public getCarById(carId) {
-        return this.cars.find(car => car.id === carId);
+    public getCarById(carId: number) {
+        const car = this.cars.find((car) => car.id === carId);
+        if(car) {
+            return car;
+        }
+        throw new HttpException('Car not found', 404);
+        // return this.cars.find((car) => car.id === carId);
     }
     public deleteCarById(carId) {
-        this.cars = this.cars.filter(car => car.id !== carId);
+        this.cars = this.cars.filter((car) => car.id !== carId);
     }
     public updateCarById(carId, car) {
-        this.cars = this.cars.map(car => car.id === carId ? car : car);
+        this.cars = this.cars.map((car) => car.id === carId ? car : car);
     }
 }
