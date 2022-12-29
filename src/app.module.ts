7df469284aa373462cfeef05af4423b0ae544396
Vcky4 <okon.victor.u@gmail.com>
2022-12-29 22:25:59 +0100

    set up typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CarModule } from './car/car.module';
 
 @Module({
-  imports: [CarModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root234',
+      database: 'car_manager',
+      entities: [],
+      synchronize: true,
+    }),
+    CarModule
+  ],
 })
-export class AppModule {}
+export class AppModule { }
