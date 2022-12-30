891abb9eb5fd071edd7f4d7c668309984ab987f7
Vcky4 <okon.victor.u@gmail.com>
2022-12-30 17:03:58 +0100

    stuff

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(private readonly authService: AuthService) { }
+
+    @Post('register')
+    async register(
+        @Body('name') name: string,
+        @Body('email') email: string,
+        @Body('password') password: string) {
+        return this.authService.register();
+    }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+    constructor(@InjectRepository(User) private readonly userRepository: Repository<User>) { }
+}
